268116a0d2b3437f5e82cdf5cb336dfc5d1a53d6
nenemizc <emarcellm@gmail.com>
2026-03-04 09:19:07 +0100

    fajl beolvasás

---
+++ Bingo/BingoJatekos.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bingo
+{
+    internal class BingoJatekos
+    {
+        private string nev;
+        private int?[,] kartya;
+        private bool[,] talalat;
+
+        public BingoJatekos(string nev, int?[,] kartya)
+        {
+            this.nev = nev;
+            this.kartya = kartya;
+            this.talalat = new bool[5, 5];
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    if (this.kartya[i, j] == null)
+                        this.talalat[i, j] = true;
+                }
+            }
+        }
+
+    }
+}
--- Bingo/Program.cs
+++ Bingo/Program.cs
@@ -1,9 +1,39 @@
-﻿namespace Bingo
+﻿using System.Collections;
+
+namespace Bingo
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            List<BingoJatekos> jatekosok = new List<BingoJatekos>();
+            string[] fajlok = File.ReadAllLines("nevek.txt");
+
+            foreach (string fajl in fajlok)
+            {
+                string nev = Path.GetFileNameWithoutExtension(fajl);
+                int?[,] kartya = BeolvasKartya(fajl);
+                jatekosok.Add(new BingoJatekos(nev, kartya));
+            }
+        }
+        static int?[,] BeolvasKartya(string fajl)
+        {
+            
+                string[] sorok = File.ReadAllLines(fajl);
+                int?[,] kartya = new int?[5, 5];
+                for (int i = 0; i < 5; i++)
+                {
+                    string[] szamok = sorok[i].Split(';');
+
+                    for (int j = 0; j < 5; j++)
+                    {
+                        if (szamok[j] == "X")
+                            kartya[i, j] = null;
+                        else
+                            kartya[i, j] = int.Parse(szamok[j]);
+                    }
+                }
+            return kartya;
         }
     }
 }
